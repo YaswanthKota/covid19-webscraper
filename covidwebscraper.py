a67cbe66a8d8cddede583d42b405d70712154cb9
Yaswanth_Kota <58515620+YaswanthKota@users.noreply.github.com>
2020-04-20 15:08:14 +0530

    Update covidwebscraper.py

--- covidwebscraper.py
+++ covidwebscraper.py
@@ -9,7 +9,7 @@ stats=[]
 rows=soup.find_all('tr')
 for row in rows:
     stat=row.find_all('td')
-    if len(stats)<=29:
+    if len(stats)<=33:
         stats.append(stat)
 new_cols = ["Sr.No", "States/UT","Confirmed","Recovered","Deceased"]
 state_data = pd.DataFrame(data = stats, columns = new_cols)
